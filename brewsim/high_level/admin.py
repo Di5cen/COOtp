249229e4176e2eb497c5d9b78b47b75833167bf6
Di5cen <123420998+Di5cen@users.noreply.github.com>
2023-09-27 21:59:21 +0200

    add high_level models & admin nffff

--- brewsim/high_level/admin.py
+++ brewsim/high_level/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Departement, Usine, Prix, Machine, Recette, Ingredient, Quantiteingredient, Action
+
+class DepartementAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish_date')
+
+class UsineAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date', 'country')
+
+class PrixAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish_date')
+
+class MachineAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date', 'country')
+
+class RecetteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish_date')
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date', 'country')
+
+class QuantiteingredientAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish_date')
+
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date', 'country')
+  
+admin.site.register(Departement, DepartementAdmin)
+admin.site.register(Usine, UsineAdmin)
+admin.site.register(Prix, PrixAdmin)
+admin.site.register(Machine, MachineAdmin)
+admin.site.register(Recette, RecetteAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Quantiteingredient, QuantiteingredientAdmin)
+admin.site.register(Action, ActionAdmin)
