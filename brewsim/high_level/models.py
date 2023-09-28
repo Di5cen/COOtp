41a0356d94f6820ae470e0ae33ef8d87c05960e4
Di5cen <123420998+Di5cen@users.noreply.github.com>
2023-09-28 16:14:54 +0200

    selfprint2

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -6,28 +6,28 @@ class Departement(models.Model):
   numero = models.IntegerField()
   prix_departement = models.FloatField()
 
-    def __str__(self):
-        return self.numero
+  def __str__(self):
+      return self.numero
 
 class Machine(models.Model):
   nom = models.CharField(max_length=200)
   prix_machine = models.FloatField()
 
-    def __str__(self):
-        return self.nom
+  def __str__(self):
+      return self.nom
 
 class Ingredient(models.Model):
   nom = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.nom
+  def __str__(self):
+      return self.nom
 
 class Quantiteingredient(models.Model):
   ingredient = models.ForeignKey(Ingredient,on_delete=models.CASCADE)
   quantite = models.IntegerField()
 
-    def __str__(self):
-        return self.ingredient
+  def __str__(self):
+      return self.ingredient
 
 class Action(models.Model):
   machines = models.ForeignKey(Machine,on_delete=models.CASCADE)
@@ -36,15 +36,15 @@ class Action(models.Model):
   ingredient = models.ForeignKey(Quantiteingredient,on_delete=models.PROTECT)
   action = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.action
+  def __str__(self):
+      return self.action
   
 class Recette(models.Model):
   nom = models.CharField(max_length=200)
   action = models.ForeignKey(Action,on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.nom
+  def __str__(self):
+      return self.nom
 
 class Usine(models.Model):
   departement = models.ForeignKey(Departement,on_delete=models.CASCADE)
@@ -53,16 +53,16 @@ class Usine(models.Model):
   recettes = models.ForeignKey(Recette,on_delete=models.PROTECT)
   stocks = models.ForeignKey(Quantiteingredient,on_delete=models.PROTECT)
 
-    def __str__(self):
-        return self.taille
+  def __str__(self):
+      return self.taille
 
 class Prix(models.Model):
   ingredient = models.ForeignKey(Ingredient,on_delete=models.CASCADE)
   departement = models.ForeignKey(Departement,on_delete=models.CASCADE)
   prix = models.FloatField()
 
-    def __str__(self):
-        return self.prix
+  def __str__(self):
+      return self.prix
 
 
 
