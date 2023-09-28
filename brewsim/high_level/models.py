245ae2139ef29a3fa590696fac9303a95c77448c
Di5cen <123420998+Di5cen@users.noreply.github.com>
2023-09-28 15:14:53 +0200

    corr3

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -6,6 +6,28 @@ class Departement(models.Model):
   numero = models.IntegerField()
   prix = models.FloatField()
 
+class Machine(models.Model):
+  nom = models.CharField(max_length=200)
+  prix = models.FloatField()
+
+class Action(models.Model):
+  machines = models.ForeignKey(Machine,on_delete=models.CASCADE)
+  commande = models.CharField(max_length=200)
+  duree = models.IntegerField()
+  ingredient = models.ForeignKey(Quantiteingredient,on_delete=models.PROTECT)
+  action = models.CharField(max_length=200)
+  
+class Recette(models.Model):
+  nom = models.CharField(max_length=200)
+  action = models.ForeignKey(Action,on_delete=models.CASCADE)
+
+class Ingredient(models.Model):
+  nom = models.CharField(max_length=200)
+
+class Quantiteingredient(models.Model):
+  ingredient = models.ForeignKey(Ingredient,on_delete=models.CASCADE)
+  quantite = models.IntegerField()
+  
 class Usine(models.Model):
   departement = models.ForeignKey(Departement,on_delete=models.CASCADE)
   taille = models.FloatField()
@@ -22,21 +44,9 @@ class Machine(models.Model):
   nom = models.CharField(max_length=200)
   prix = models.FloatField()
 
-class Recette(models.Model):
-  nom = models.CharField(max_length=200)
-  action = models.FloatField(Action,on_delete=models.CASCADE)
-
 class Ingredient(models.Model):
   nom = models.CharField(max_length=200)
 
-class Quantiteingredient(models.Model):
-  ingredient = models.ForeignKey(Ingredient,on_delete=models.CASCADE)
-  quantite = models.IntegerField()
 
-class Action(models.Model):
-  machines = models.ForeignKey(Machine,on_delete=models.CASCADE)
-  commande = models.CharField(max_length=200)
-  duree = models.IntegerField()
-  ingredient = models.ForeignKey(Quantiteingredient,on_delete=models.PROTECT)
-  def __str__(self):
-        return 
+
+
