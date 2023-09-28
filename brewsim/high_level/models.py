f760b3051fa6cb7166c35727f1eb1240e3cc6208
Di5cen <123420998+Di5cen@users.noreply.github.com>
2023-09-28 15:01:08 +0200

    corrc1

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Departement(models.Model):
-  numero = models.IntergerField()
+  numero = models.IntegerField()
   prix = models.ForeignKey(Prix,on_delete=models.CASCADE)
 
 class Usine(models.Model):
@@ -19,24 +19,24 @@ class Prix(models.Model):
   prix = models.FloatField()
 
 class Machine(models.Model):
-  nom = models.CharField()
+  nom = models.CharField(max_length=200)
   prix = models.FloatField()
 
 class Recette(models.Model):
-  nom = models.CharField()
+  nom = models.CharField(max_length=200)
   action = models.FloatField(Action,on_delete=models.CASCADE)
 
 class Ingredient(models.Model):
-  nom = models.CharField()
+  nom = models.CharField(max_length=200)
 
 class Quantiteingredient(models.Model):
   ingredient = models.ForeignKey(Ingredient,on_delete=models.CASCADE)
-  quantite = models.IntergerField()
+  quantite = models.IntegerField()
 
 class Action(models.Model):
   machines = models.ForeignKey(Machine,on_delete=models.CASCADE)
-  commande = models.CharField()
-  duree = models.IntergerField()
+  commande = models.CharField(max_length=200)
+  duree = models.IntegerField()
   ingredient = models.ForeignKey(Quantiteingredient,on_delete=models.PROTECT)
   def __str__(self):
         return 
