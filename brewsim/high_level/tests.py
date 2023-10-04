7fff716157d1f6b5c207418330554ef2837bff78
Di5cen <123420998+Di5cen@users.noreply.github.com>
2023-10-04 09:01:54 +0200

    Update test1_2.py

--- brewsim/high_level/tests.py
+++ brewsim/high_level/tests.py
@@ -7,5 +7,5 @@ from .models import Machine
 class MachineModelTests(TestCase):
   def test_usine_creation(self):
     self.assertEqual(Machine.objects.count(), 0)
-    Machine.objects.create(nom="four",prix=1_000)
+    Machine.objects.create(nom="four",prix_machine=1_000)
     self.assertEqual(Machine.objects.count(), 1)
