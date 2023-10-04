46e9e254cdbe6e7cc63a0ffaa23de12b944bcad5
Di5cen <123420998+Di5cen@users.noreply.github.com>
2023-10-04 11:07:59 +0200

    Update models_json1.py

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models , json
 
 # Create your models here.
 
@@ -9,6 +9,13 @@ class Departement(models.Model):
   def __str__(self):
       return str(self.numero)
 
+  def json(self):
+        return {
+            'id': self.id,
+            'numero': str(self.numero),
+            'prix_departement': str(self.prix_departement),
+        }
+
 class Machine(models.Model):
   nom = models.CharField(max_length=200)
   prix_machine = models.FloatField()
@@ -16,12 +23,25 @@ class Machine(models.Model):
   def __str__(self):
       return self.nom
 
+def json(self):
+        return {
+            'id': self.id,
+            'nom': self.nom,
+            'prix_machine': str(self.prix_machine),
+        }
+  
 class Ingredient(models.Model):
   nom = models.CharField(max_length=200)
 
   def __str__(self):
       return self.nom
 
+  def json(self):
+        return {
+            'id': self.id,
+            'nom': self.nom,
+        }
+    
 class Quantiteingredient(models.Model):
   ingredient = models.ForeignKey(Ingredient,on_delete=models.CASCADE)
   quantite = models.IntegerField()
@@ -29,6 +49,13 @@ class Quantiteingredient(models.Model):
   def __str__(self):
       return str(self.ingredient)
 
+  def json(self):
+        return {
+            'id': self.id,
+            'ingredient': self.ingredient.nom,
+            'quantite': str(self.quantite),
+        }
+    
 class Action(models.Model):
   machines = models.ForeignKey(Machine,on_delete=models.CASCADE)
   commande = models.CharField(max_length=200)
@@ -38,6 +65,16 @@ class Action(models.Model):
 
   def __str__(self):
       return self.action
+
+  def json(self):
+        return {
+            'id': self.id,
+            'machines': self.machines.nom,
+            'commande': self.quantite,
+            'duree': str(self.duree),
+            'ingredient': self.ingredient.nom,
+            'action': self.action,
+        }
   
 class Recette(models.Model):
   nom = models.CharField(max_length=200)
@@ -46,6 +83,13 @@ class Recette(models.Model):
   def __str__(self):
       return self.nom
 
+  def json(self):
+        return {
+            'id': self.id,
+            'nom': self.nom,
+            'action': self.action.action,
+        }
+   
 class Usine(models.Model):
   departement = models.ForeignKey(Departement,on_delete=models.CASCADE)
   taille = models.FloatField()
@@ -56,6 +100,16 @@ class Usine(models.Model):
   def __str__(self):
       return str(self.taille)
 
+  def json(self):
+        return {
+            'id': self.id,
+            'departement': str(self.departement.numero),
+            'taille': str(self.taille),
+            'machines': str(self.machines.nom),
+            'recettes': self.recettes.nom,
+            'stocks': self.stocks.ingredient,
+        }
+ 
 class Prix(models.Model):
   ingredient = models.ForeignKey(Ingredient,on_delete=models.CASCADE)
   departement = models.ForeignKey(Departement,on_delete=models.CASCADE)
@@ -64,6 +118,14 @@ class Prix(models.Model):
   def __str__(self):
       return str(self.prix)
 
+  def json(self):
+        return {
+            'id': self.id,
+            'ingredient': self.ingredient.nom,
+            'departement': self.departement.nom,
+            'prix': str(self.prix),
+        }
+ 
 
 
 
