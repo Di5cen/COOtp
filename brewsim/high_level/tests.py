34820e26c6f62f889a5229cf1becc2f2abe3b235
Di5cen <123420998+Di5cen@users.noreply.github.com>
2023-10-04 08:59:40 +0200

    Update test1.py

--- brewsim/high_level/tests.py
+++ brewsim/high_level/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+# test1:测试Machine模型的创建
+from .models import Machine
+
+class MachineModelTests(TestCase):
+  def test_usine_creation(self):
+    self.assertEqual(Machine.objects.count(), 0)
+    Machine.objects.create(nom="four",prix=1_000)
+    self.assertEqual(Machine.objects.count(), 1)
