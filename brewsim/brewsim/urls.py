21313eef7116dc1db273642924928a49deca033f
Di5cen <123420998+Di5cen@users.noreply.github.com>
2023-10-04 11:58:06 +0200

    Update urls23.py

--- brewsim/brewsim/urls.py
+++ brewsim/brewsim/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('admin/departement/<int:pk>/'),
+    path('admin/departement/<int:pk>/', views.DepartementDetailView, name='departement-detail'),
 ]
