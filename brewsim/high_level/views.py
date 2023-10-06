d6c491f860249e0b617cc79839432e272e40f9d7
Gao <gyx186239@163.com>
2023-10-06 10:03:18 +0200

    add json

--- brewsim/high_level/views.py
+++ brewsim/high_level/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Departement
 # Create your views here.
-class DepartementDetailView(DetailView)
+class DepartementDetailView(DetailView):
   model = Departement
-  def reponse(self)
-  return HttpResponse(dumps()self.object.json()) 
+  def reponse(self):
+  	return HttpResponse(dumps(self.object.json()) )
